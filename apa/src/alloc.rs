ba73223e0ed149261704fd4113479b8ad216ac21
James Whaley <juicy66173@gmail.com>
2020-10-13 12:09:19 +0100

    Split apa into separate repository

--- apa/src/alloc.rs
+++
@@ -1,9 +0,0 @@
-cfg_if::cfg_if! {
-    if #[cfg(feature = "std")] {
-        pub use std::alloc::{alloc, alloc_zeroed, dealloc, handle_alloc_error, realloc};
-    } else {
-        extern crate alloc;
-
-        pub use alloc::alloc::{alloc, alloc_zeroed, dealloc, handle_alloc_error, realloc};
-    }
-}
